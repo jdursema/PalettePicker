3b6676838b7869b6333d5fa5d086956b9e94e8d5
Julie Dursema <jmdursema@gmail.com>
2018-01-22 15:34:29 -0700

    add color divs, generate button, save button, palette name input and style

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(app.get('port'), () => {
+  console.log('is listening')
+})
