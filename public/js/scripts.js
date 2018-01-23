a739a318853cb75b8591210e02e154011040ea57
Julie Dursema <jmdursema@gmail.com>
2018-01-23 16:27:25 -0700

    collapse projects menu on click

---
+++ public/js/scripts.js
@@ -0,0 +1,36 @@
+const generateColor = () => {
+  let hex = '#'
+  const values = 'ABCDEF0123456789';
+    while(hex.length< 7){
+      let randomIndex = Math.floor(Math.random() * 16)
+      hex = hex.concat(values[randomIndex])
+    } 
+    return hex; 
+}
+
+const generatePalette = () => {
+  const boxArray = $('.color')
+  boxArray.each(function() {
+    let color = generateColor()
+    $( this ).css('background-color', color)
+    $(this).next().text(color)
+    
+  })
+}
+
+const openMenu = () => {
+  if($('.projects').hasClass('projects-hiden')) {
+    $('.projects').removeClass('projects-hiden')
+    $('.projects').addClass('projects-shown')
+  } else {
+    $('.projects').removeClass('projects-shown')
+    $('.projects').addClass('projects-hiden') 
+  }
+}
+
+
+
+
+$('document').ready(generatePalette);
+$('.menu-icon').on('click', openMenu)
+$('.generate-btn').on('click', generatePalette);
